1c61f80416d16afbad4262170c2e7195237ecc6f
deuteri <hoangdinhhieupk@gmail.com>
2022-01-10 12:10:05 +0700

    update page scroll percent

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -206,13 +206,13 @@ window.onload = function() {
 
 
 let scrollPercentage = () => {
-    let scrollProgress = document.getElementById("progress");
-    let progressValue = document.getElementById("progress-value");
+    let scrollProgress = document.getElementById("scroll-progress");
+    let progressValue = document.getElementById("scroll-progress-value");
     let pos = document.documentElement.scrollTop;
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrollValue = Math.round( pos * 100 / calcHeight);
 
-    scrollProgress.style.background = `conic-gradient(#008fff ${scrollValue}%, #c0c0ff ${scrollValue}%)`;
+    scrollProgress.style.background = `conic-gradient(#bb86fc ${scrollValue}%, #c0c0ff ${scrollValue}%)`;
     progressValue.textContent = `${scrollValue}%`;
 }
 
